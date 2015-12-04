0cf182a0fe9e7436716b6ed8a5bd63e382fb04d2
Paul Water <p.r.water@setbased.nl>
2015-12-04 15:29:09 +0100

    Added interface ReferenceObfuscatorMangler.

---
+++ src/Obfuscator/ReferenceObfuscatorMangler.php
@@ -0,0 +1,30 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\Obfuscator;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Interface for deriving labels from table metadata.
+ */
+interface ReferenceObfuscatorMangler
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the label of a table based on the metadata of the table.
+   *
+   * @param array $theTable The metadata of the table. The array has the following keys:
+   *                        <ul>
+   *                        <li> table_name   The name of the table.
+   *                        <li> column_name  The name of the autoincrement column.
+   *                        <li> column_type  The data type of the autoincrement column.
+   *                        </ul>
+   *
+   * @return string
+   */
+  public function getLabel($theTable);
+
+  //-------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
